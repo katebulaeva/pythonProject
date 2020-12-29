e84a62c517c2cf9e5d9960bfb701df4790ea63ea
katebulaeva <katebulaeva@gmail.com>
2020-12-29 20:11:01 +0300

    l2-1

---
+++ L2_1.py
@@ -0,0 +1,17 @@
+a = int(22)
+b = float(2.2)
+c = str('abc')
+d = ['abc', 1, 2.2, "Hello"]
+e = {"name" : 'Kate', 'age' : 33, 'sex' : 'Female'}
+f = [1, 2, 'Hello']
+g = (1, 3, 'Hello')
+h = bool(2 == 2)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
